17ae4ef3dd4f760e1c67516eba7f41c7c00f9ca3
Ali Kaif <aliakaif@email.arizona.edu>
2022-08-29 14:09:04 -0700

    Update script.js
    Fixed a cache loader issue where images in portfolio would overlap

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+$(window).on("load", function() {
+
+	$(".loader .inner").fadeOut(500, function() {
+		$(".loader").fadeOut(750);
+	});
+
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+
+	})
+})
+
+
 $(document).ready(function() {
 
 	$('#slides').superslides({
